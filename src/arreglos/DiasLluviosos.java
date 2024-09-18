fd7f29a78c4c4d39e801f3cb38cc6c261d8b7a49
Nicolás <nicolascesarini94@hotmail.com>
2024-09-18 19:53:29 -0300

    Ejercicio DiasLluviosos realizado

--- src/arreglos/DiasLluviosos.java
+++ src/arreglos/DiasLluviosos.java
@@ -26,10 +26,53 @@ public class DiasLluviosos {
         this.diasLluviosos[diaDeAnio - 1] = true;
     }
 
-    /*boolean consultarSiLlovioElDia(int diaDelAnio){
-        if(diaDelAnio < 1 || diaDelAnio > 365){}
-    }*/
+    boolean consultarSiLlovioElDia(int diaDelAnio){
+        if(diaDelAnio < 1 || diaDelAnio > 365){
+            throw new Error("El dia del año debe estar comprendido entre 1 y 365");
+        }
+        return this.diasLluviosos[diaDelAnio - 1];
+    }
 
+    int contarDiasLluviosos(){
+        int cantidadDiasLluviosos = 0;
+        for(int i=0; i<this.diasLluviosos.length; i++){
+            if(diasLluviosos[i]){
+                cantidadDiasLluviosos += 1;
+            }
+        }
+        return cantidadDiasLluviosos;
+    }
 
+    int consultarCuatrimestreMasLluvioso(){
+        int cantidadDiasLluviososPrimerCuatrimestre = 0;
+        int cantidadDiasLluviososSegundoCuatrimestre = 0;
+        int cantidadDiasLluviososTercerCuatrimestre = 0;
+        int cuatrimestreMasLluvioso = 1;
+        for(int i=0; i<this.diasLluviosos.length; i++){
+            if(this.diasLluviosos[i] && (i<(this.diasLluviosos.length/3))){
+                cantidadDiasLluviososPrimerCuatrimestre += 1;
+            } else if (this.diasLluviosos[i] && (i<((2*this.diasLluviosos.length)/3))){
+                cantidadDiasLluviososSegundoCuatrimestre += 1;
+            } else if (this.diasLluviosos[i]){
+                cantidadDiasLluviososTercerCuatrimestre += 1;
+            }
+        }
+        if(cantidadDiasLluviososSegundoCuatrimestre > cantidadDiasLluviososPrimerCuatrimestre
+                && cantidadDiasLluviososSegundoCuatrimestre > cantidadDiasLluviososTercerCuatrimestre){
+            cuatrimestreMasLluvioso = 2;
+        }
+        if (cantidadDiasLluviososTercerCuatrimestre > cantidadDiasLluviososPrimerCuatrimestre
+                && cantidadDiasLluviososTercerCuatrimestre > cantidadDiasLluviososSegundoCuatrimestre){
+            cuatrimestreMasLluvioso = 3;
+        }
+        return cuatrimestreMasLluvioso;
+    }
 
+    int consultarPrimerDiaConLluvia(){
+        int i = 0;
+        while (!diasLluviosos[i]){
+            i += 1;
+        }
+        return i+1;
+    }
 }
